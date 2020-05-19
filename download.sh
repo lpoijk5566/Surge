bb159cb85e827d6b7376c0d4f1fda53d991739b1
Pan, Kris <kris.pan@intel.com>
2020-05-19 10:19:27 +0800

    Add download script

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh && chmod a+x *
+
+exit 0
